a7bf59e6261b4638155045c2971ae9430d6a20ad
Joshua Corpin <jkcorpin@gmail.com>
2020-12-03 17:39:06 +0800

    Setup DB and middlewares

--- server/app.js
+++ server/app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const authRouter = require('./routes/authRoutes');
+const clientRouter = require('./routes/clientRoutes');
+
 const app = express();
+
+if (process.env.ENV === 'development') app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/clients', clientRouter);
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('connected to DB'));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running at port ${process.env.PORT}`)
