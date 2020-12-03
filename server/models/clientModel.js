f5097359a4ec3d86eecbb2c84e029c9385403ec6
Joshua Corpin <jkcorpin@gmail.com>
2020-12-03 17:40:16 +0800

    Added pre middlewares and instance methods

---
+++ server/models/clientModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const clientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name'],
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: [true, 'Email already exist'],
+    required: [true, 'Please provide your email'],
+    validate: [validator.isEmail, 'Please provide a valid email.'],
+  },
+  password: {
+    type: String,
+    select: false,
+    minlength: [8, 'Password must have atleast 8 characters'],
+    required: [true, 'Please provide your password'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (inputPassword) {
+        return inputPassword === this.password;
+      },
+      message: "Password don't match",
+    },
+  },
+  users: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+clientSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+clientSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'users',
+    select: '-__v -client',
+  });
+
+  next();
+});
+
+clientSchema.methods.isPasswordCorrect = async function (
+  inputPassword,
+  userPassword
+) {
+  return await bcrypt.compare(inputPassword, userPassword);
+};
+
+module.exports = mongoose.model('Client', clientSchema);
