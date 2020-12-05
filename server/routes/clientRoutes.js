1e4c342f4a5db11e23a27d81883289c548bec105
Joshua Corpin <jkcorpin@gmail.com>
2020-12-05 10:24:28 +0800

    Endpoint for updating and reading of client users

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -6,11 +6,17 @@ const clientController = require('../controllers/clientController');
 const Router = express.Router();
 
 Router.get('/', clientController.getAllClients);
-Router.patch('/add-user', authController.protect, clientController.addUser);
-Router.post(
-  '/send-email',
-  authController.protect,
-  clientController.sendEmailToClients
-);
+
+Router.use(authController.protect);
+
+Router.post('/send-email', clientController.sendEmailToClients);
+
+Router.route('/users/:userid')
+  .get(clientController.getUser)
+  .patch(clientController.updateUser);
+
+Router.route('/users')
+  .get(clientController.getAllUsers)
+  .post(clientController.addUser);
 
 module.exports = Router;
