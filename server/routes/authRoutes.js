518e857a1e0c008503ab4e976061225594e25dad
Joshua Corpin <jkcorpin@gmail.com>
2020-12-09 09:42:58 +0800

    Added route for forgot and reset password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,8 @@ const Router = express.Router();
 
 Router.post('/register', authController.register);
 Router.post('/login', authController.login);
+Router.post('/forgot-password', authController.forgotPassword);
 Router.post('/verify/:token', authController.verifyEmail);
+Router.post('/reset-password/:token', authController.resetPassword);
 
 module.exports = Router;
