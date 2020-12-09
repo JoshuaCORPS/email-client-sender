3ea05583a3d3a446bbb36333f8a633f72cb6bdd9
Joshua Corpin <jkcorpin@gmail.com>
2020-12-09 11:54:18 +0800

    Implemented catchasync for auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,8 +2,10 @@ const { promisify } = require('util');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
-const sendEmail = require('../util/email');
 const Client = require('../models/clientModel');
+
+const sendEmail = require('../util/email');
+const catchAsync = require('../util/catchAsync');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -33,75 +35,61 @@ const createSendCookieTokenResponse = (client, statusCode, res, req) => {
   });
 };
 
-exports.register = async (req, res) => {
-  try {
-    const verifyToken = crypto.randomBytes(32).toString('hex');
-    const hashedToken = crypto
-      .createHash('sha256')
-      .update(verifyToken)
-      .digest('hex');
+exports.register = catchAsync(async (req, res, next) => {
+  const verifyToken = crypto.randomBytes(32).toString('hex');
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(verifyToken)
+    .digest('hex');
 
-    const client = await Client.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-      emailVerifyToken: hashedToken,
+  const client = await Client.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    emailVerifyToken: hashedToken,
+  });
+
+  const verifyURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/verify/${verifyToken}`;
+
+  const message = `To activate your account, please verify your email address.\nYour account will not be created until your email address is confirmed.\n\nClick this link to activate your account: ${verifyURL}`;
+
+  await sendEmail({
+    from: 'Sender account@sender.com',
+    email: req.body.email,
+    subject: 'Verify Your Account',
+    message,
+  });
+
+  createSendCookieTokenResponse(client, 201, res, req);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provide your email and password to proceed',
     });
 
-    const verifyURL = `${req.protocol}://${req.get(
-      'host'
-    )}/api/v1/auth/verify/${verifyToken}`;
+  const client = await Client.findOne({
+    email,
+    active: { $ne: false },
+  }).select('+password');
 
-    const message = `To activate your account, please verify your email address.\nYour account will not be created until your email address is confirmed.\n\nClick this link to activate your account: ${verifyURL}`;
-
-    await sendEmail({
-      from: 'Sender account@sender.com',
-      email: req.body.email,
-      subject: 'Verify Your Account',
-      message,
+  if (!client || !(await client.isPasswordCorrect(password, client.password)))
+    return res.status(401).json({
+      status: 'fail',
+      message: 'Incorrect email or password',
     });
 
-    createSendCookieTokenResponse(client, 201, res, req);
-  } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
+  createSendCookieTokenResponse(client, 200, res, req);
+});
 
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password)
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Please provide your email and password to proceed',
-      });
-
-    const client = await Client.findOne({
-      email,
-      active: { $ne: false },
-    }).select('+password');
-
-    if (!client || !(await client.isPasswordCorrect(password, client.password)))
-      return res.status(401).json({
-        status: 'fail',
-        message: 'Incorrect email or password',
-      });
-
-    createSendCookieTokenResponse(client, 200, res, req);
-  } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
-
-exports.protect = async (req, res, next) => {
+exports.protect = catchAsync(async (req, res, next) => {
   let token;
 
   if (
@@ -134,7 +122,7 @@ exports.protect = async (req, res, next) => {
 
   req.client = client;
   next();
-};
+});
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
@@ -148,138 +136,116 @@ exports.restrictTo = (...roles) => {
   };
 };
 
-exports.verifyEmail = async (req, res) => {
-  try {
-    const hashedToken = crypto
-      .createHash('sha256')
-      .update(req.params.token)
-      .digest('hex');
+exports.verifyEmail = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
 
-    const client = await Client.findOne({
-      emailVerifyToken: hashedToken,
-      active: false,
+  const client = await Client.findOne({
+    emailVerifyToken: hashedToken,
+    active: false,
+  });
+
+  if (!client)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid token',
     });
 
-    if (!client)
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Invalid token',
-      });
+  client.active = true;
+  client.emailVerifyToken = undefined;
 
-    client.active = true;
-    client.emailVerifyToken = undefined;
+  await client.save({ validateBeforeSave: false });
 
-    await client.save({ validateBeforeSave: false });
+  res.status(200).json({
+    status: 'success',
+    message: 'Account successfully activated',
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      message: 'Account successfully activated',
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provide your email to reset your password',
+    });
+
+  const client = await Client.findOne({ email, active: { $ne: false } });
+
+  if (!client)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'client not found',
+    });
+
+  const resetToken = client.createPasswordResetToken();
+  await client.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/reset-password/${resetToken}`;
+
+  const message = `Hi ${client.name}.\n\nForgot your password? To reset your password, go to this link: ${resetURL}.`;
+
+  try {
+    await sendEmail({
+      from: 'Sender support@sender.com',
+      email,
+      subject: 'Your password reset token (valid for only 10 minutes)',
+      message,
     });
   } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
-
-exports.forgotPassword = async (req, res) => {
-  try {
-    const { email } = req.body;
-
-    if (!email)
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Please provide your email to reset your password',
-      });
-
-    const client = await Client.findOne({ email });
-
-    if (!client)
-      return res.status(404).json({
-        status: 'fail',
-        message: 'client not found',
-      });
-
-    const resetToken = client.createPasswordResetToken();
-    await client.save({ validateBeforeSave: false });
-
-    const resetURL = `${req.protocol}://${req.get(
-      'host'
-    )}/api/v1/auth/reset-password/${resetToken}`;
-
-    const message = `Hi ${client.name}.\n\nForgot your password? To reset your password, go to this link: ${resetURL}.`;
-
-    try {
-      await sendEmail({
-        from: 'Sender support@sender.com',
-        email,
-        subject: 'Your password reset token (valid for only 10 minutes)',
-        message,
-      });
-    } catch (error) {
-      client.passwordResetToken = undefined;
-      client.passwordResetExpires = undefined;
-
-      await client.save({ validateBeforeSave: false });
-
-      res.status(500).json({
-        status: 'error',
-        message:
-          'There was an error sending the email. Please try again later!',
-      });
-    }
-
-    res.status(200).json({
-      status: 'success',
-      message: 'Token sent to mail',
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
-
-exports.resetPassword = async (req, res) => {
-  try {
-    const { password, passwordConfirm } = req.body;
-
-    if (!password || !passwordConfirm)
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Please provide your password to proceed',
-      });
-
-    const hashedToken = crypto
-      .createHash('sha256')
-      .update(req.params.token)
-      .digest('hex');
-
-    const client = await Client.findOne({
-      passwordResetToken: hashedToken,
-      passwordResetExpires: { $gt: Date.now() },
-    });
-
-    if (!client)
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Token is invalid or has expired',
-      });
-
-    client.password = password;
-    client.passwordConfirm = passwordConfirm;
     client.passwordResetToken = undefined;
     client.passwordResetExpires = undefined;
 
-    await client.save();
+    await client.save({ validateBeforeSave: false });
 
-    createSendCookieTokenResponse(client, 200, res, req);
-  } catch (error) {
     res.status(500).json({
       status: 'error',
-      message: error.message,
+      message: 'There was an error sending the email. Please try again later!',
     });
   }
-};
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Token sent to mail',
+  });
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const { password, passwordConfirm } = req.body;
+
+  if (!password || !passwordConfirm)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provide your password to proceed',
+    });
+
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const client = await Client.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!client)
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Token is invalid or has expired',
+    });
+
+  client.password = password;
+  client.passwordConfirm = passwordConfirm;
+  client.passwordResetToken = undefined;
+  client.passwordResetExpires = undefined;
+
+  await client.save();
+
+  createSendCookieTokenResponse(client, 200, res, req);
+});
