15702057d522871c14d37f8a5201f31caeb753f5
Joshua Corpin <jkcorpin@gmail.com>
2020-12-10 11:28:07 +0800

    Enabled cors and implement basic securities

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
+const compression = require('compression');
 
 const authRouter = require('./routes/authRoutes');
 const clientRouter = require('./routes/clientRoutes');
@@ -12,9 +20,29 @@ const AppError = require('./util/appError');
 
 const app = express();
 
+app.enable('trust proxy');
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+app.use(
+  '/api',
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request! Please try again later.',
+  })
+);
+
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(mongoSanitize());
+app.use(xss());
+
+app.use(compression());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/clients', clientRouter);
