a2b8f3f64ee32e8cfa3b6e1a0d1470705408239e
Joshua Corpin <jkcorpin@gmail.com>
2020-12-11 17:48:57 +0800

    Create simple login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Form, Input, Button, Row, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
-function App() {
+const { Title } = Typography;
+
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputEmailOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const inputPasswordOnChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submitFormData = async () => {
+    try {
+      const client = await axios.post(
+        "http://127.0.0.1:5000/api/v1/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(client);
+    } catch (error) {
+      // alert(error.response.data.message);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Row
+      align="middle"
+      justify="center"
+      style={{
+        minHeight: "75vh",
+      }}
+    >
+      <Form
+        style={{
+          border: "1px solid #40a9ff",
+          padding: "100px",
+          width: "35rem",
+          top: "50%",
+        }}
+        name="normal_login"
+        className="login-form"
+        onFinish={submitFormData}
+      >
+        <Title level={3}>Log in</Title>
+
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: "Please input your Email!" },
+            { type: "email" },
+          ]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Input
+            onChange={inputEmailOnChange}
+            size="large"
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input.Password
+            onChange={inputPasswordOnChange}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            size="large"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Button type="primary" htmlType="submit" block>
+          Log in
+        </Button>
+        <Row justify="center">
+          <Form.Item>
+            <a href="">Register</a>
+          </Form.Item>
+        </Row>
+        <Row justify="center">
+          <Form.Item>
+            <a className="login-form-forgot" href="">
+              Forgot password?
+            </a>
+          </Form.Item>
+        </Row>
+      </Form>
+    </Row>
   );
-}
+};
 
 export default App;
