d01ece91e62225ff6cce187e861808be034201ac
Joshua Corpin <jkcorpin@gmail.com>
2020-12-13 17:52:35 +0800

    changed domain in cookie option

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,7 +18,7 @@ const createSendCookieTokenResponse = (client, statusCode, res, req) => {
   const token = signToken(client._id);
 
   const cookieOption = {
-    domain: '.herokuapp.com',
+    domain: 'localhost',
     path: '/',
     httpOnly: true,
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
