4a82608d8b5b184d3a17323097afa555f2d2196e
Joshua Corpin <jkcorpin@gmail.com>
2020-12-13 16:32:13 +0800

    Added router for reset password and verify account

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./Containers/Login/Login";
 import Signup from "./Containers/Signup/Signup";
 import ForgotPassword from "./Containers/ForgotPassword/ForgotPassword";
+import ResetPassword from "./Containers/ResetPassword/ResetPassword";
+import VerifyAccount from "./Containers/VerifyAccount/VerifyAccount";
 
 const App = () => {
   return (
     <main>
       <Switch>
+        <Route path="/verify/:token" component={VerifyAccount} />
+        <Route path="/reset-password/:token" component={ResetPassword} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/forgot-password" component={ForgotPassword} />
