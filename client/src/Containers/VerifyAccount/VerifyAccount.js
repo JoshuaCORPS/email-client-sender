b104d58e23717007d794a2528d42430a54a75665
Joshua Corpin <jkcorpin@gmail.com>
2020-12-13 16:27:51 +0800

    Added Container for Verifying Account

---
+++ client/src/Containers/VerifyAccount/VerifyAccount.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import { Alert, Row } from "antd";
+
+import classes from "./VerifyAccount.module.css";
+
+const VerifyAccount = (props) => {
+  console.log(props.match.params.token);
+
+  useEffect(() => {
+    try {
+      const verifyToken = async () => {
+        try {
+          const result = await axios.post(
+            `/api/v1/auth/verify/${props.match.params.token}`
+          );
+
+          if (result.data.status === "success") {
+            ReactDOM.render(
+              <Alert
+                message="Success"
+                description={result.data.message}
+                type="success"
+                showIcon
+              />,
+              document.getElementById("alert")
+            );
+            setTimeout(() => {
+              window.location.assign("/login");
+            }, 2000);
+          }
+        } catch (error) {
+          ReactDOM.render(
+            <Alert
+              message="Failed"
+              description="Account token is not valid!"
+              type="error"
+              showIcon
+            />,
+            document.getElementById("alert")
+          );
+          setTimeout(() => {
+            window.location.assign("/signup");
+          }, 2000);
+        }
+      };
+      verifyToken();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <Row justify="center" align="middle" className={classes.RowVH}>
+      <div id="alert"></div>
+    </Row>
+  );
+};
+
+export default withRouter(VerifyAccount);
