636f7b7509a74e4c69313542b4e4c12e7d5daf9c
Joshua Corpin <jkcorpin@gmail.com>
2020-12-13 18:25:01 +0800

    Removed domain and path to cookie option

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,8 +18,6 @@ const createSendCookieTokenResponse = (client, statusCode, res, req) => {
   const token = signToken(client._id);
 
   const cookieOption = {
-    domain: '.vercel.app',
-    path: '/',
     httpOnly: true,
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
