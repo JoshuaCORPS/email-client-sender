ad92f25218212a07b88959c42a451ac592dcc7ec
Joshua Corpin <jkcorpin@gmail.com>
2020-12-14 20:09:20 +0800

    Added handler for sending 200 request

--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -3,6 +3,8 @@ const authController = require('../controllers/authController');
 
 const Router = express.Router();
 
-Router.get('/', authController.protect);
+Router.get('/', authController.protect, (req, res) =>
+  res.status(200).json({ status: 'success' })
+);
 
 module.exports = Router;
