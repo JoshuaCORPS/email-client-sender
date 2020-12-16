9bd4e037ee8d7936127f8ff6899cce0b73f351ad
Joshua Corpin <jkcorpin@gmail.com>
2020-12-16 18:42:24 +0800

    Added component for subject input

---
+++ client/src/Components/Form/InputSubject/InputSubject.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Form, Input } from "antd";
+
+const inputSubject = ({ value, handleChange }) => {
+  return (
+    <>
+      {/* For Subject */}
+      <Form.Item
+        name="itemSubject"
+        rules={[{ required: true, message: "Please input the Subject!" }]}
+      >
+        <Input
+          name="subject"
+          value={value.subject}
+          onChange={handleChange}
+          size="large"
+          placeholder="Subject"
+        />
+      </Form.Item>
+    </>
+  );
+};
+
+export default inputSubject;
