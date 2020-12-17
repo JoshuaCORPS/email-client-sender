27075c89d2a1f9035a9d9647201f5c3ad0bf28b6
Joshua Corpin <jkcorpin@gmail.com>
2020-12-17 15:09:18 +0800

    Added route for mailusers,adduser,dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,31 @@ import Signup from "./Containers/Signup/Signup";
 import ForgotPassword from "./Containers/ForgotPassword/ForgotPassword";
 import ResetPassword from "./Containers/ResetPassword/ResetPassword";
 import VerifyAccount from "./Containers/VerifyAccount/VerifyAccount";
-import Dashboard from "./Components/Dashboard/Dashboard";
+import Dashboard from "./Containers/Dashboard/Dashboard";
+import MailUsers from "./Containers/MailUsers/MailUsers";
+import AddUser from "./Containers/AddUser/AddUser";
 
 const App = () => {
   return (
     <main>
       <Switch>
+        <Route
+          path="/mail/mail-users"
+          render={() => <Dashboard content={<MailUsers />} />}
+        />
+        <Route
+          path="/users/add-user"
+          render={() => <Dashboard content={<AddUser />} />}
+        />
         <Route path="/verify/:token" component={VerifyAccount} />
         <Route path="/reset-password/:token" component={ResetPassword} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/forgot-password" component={ForgotPassword} />
-        <Route path="/" component={Dashboard} />
+        <Route
+          path="/"
+          render={() => <Dashboard content={<h3>Dashboard</h3>} />}
+        />
       </Switch>
     </main>
   );
