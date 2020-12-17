c8d8c1932844743e92ab537a3ccd245c95a699c0
Joshua Corpin <jkcorpin@gmail.com>
2020-12-17 16:37:50 +0800

    Added secure and same site options in logout cookie response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -98,6 +98,8 @@ exports.logout = (req, res) => {
   res.cookie('jwt', '', {
     expires: new Date(Date.now() + 1 * 1000),
     httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    sameSite: 'none',
   });
 
   res.status(200).json({
