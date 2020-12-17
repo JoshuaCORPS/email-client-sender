8c590eed3e14493ed85900758f945a61cf413e7d
Joshua Corpin <jkcorpin@gmail.com>
2020-12-17 15:51:08 +0800

    Set logout cookie expires in 1sec

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -96,7 +96,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie('jwt', '', {
-    expires: new Date(Date.now() + 10 * 1000),
+    expires: new Date(Date.now() + 1 * 1000),
     httpOnly: true,
   });
 
