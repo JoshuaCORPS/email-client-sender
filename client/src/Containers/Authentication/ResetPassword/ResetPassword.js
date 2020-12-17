4278f8ac48db7782b59458718d358026baaa7270
Joshua Corpin <jkcorpin@gmail.com>
2020-12-17 16:12:14 +0800

    Group containers by authentication

---
+++ client/src/Containers/Authentication/ResetPassword/ResetPassword.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { useForm } from "../../../hooks/useForm";
+import { withRouter } from "react-router-dom";
+import { Form, Button, Row, Typography, Alert } from "antd";
+
+import InputPassword from "../../../Components/Form/InputPassword/InputPassword";
+import InputPasswordConfirm from "../../../Components/Form/InputPasswordConfirm/InputPasswordConfirm";
+import Spinner from "../../../Components/Spinner/Spinner";
+
+import classes from "./ResetPassword.module.css";
+
+const { Title } = Typography;
+
+const ResetPassword = (props) => {
+  const [values, handleChange] = useForm({ password: "", passwordConfirm: "" });
+  const [isTokenVerified, setIsTokenVerified] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitFormData = async () => {
+    try {
+      setLoading(true);
+
+      const searchClient = await axios.post(
+        `/api/v1/auth/reset-password/${props.match.params.token}`,
+        values
+      );
+
+      if (searchClient.data.status === "success")
+        ReactDOM.render(
+          <Alert message="Success" type="success" showIcon />,
+          document.getElementById("alert")
+        );
+
+      setLoading(false);
+
+      window.setTimeout(() => {
+        window.location.assign("/login");
+      }, 2000);
+    } catch (error) {
+      ReactDOM.render(
+        <Alert message={error.response.data.message} type="error" showIcon />,
+        document.getElementById("alert")
+      );
+
+      setLoading(false);
+
+      setTimeout(() => {
+        ReactDOM.render("", document.getElementById("alert"));
+      }, 3000);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      const verifyResetToken = async () => {
+        try {
+          const result = await axios.get(
+            `/api/v1/auth/reset-password/${props.match.params.token}`
+          );
+
+          if (result.data.status === "success") setIsTokenVerified(true);
+        } catch (error) {
+          window.location.assign("/");
+        }
+      };
+
+      verifyResetToken();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [props.match.params.token]);
+
+  let content = <Spinner />;
+
+  if (isTokenVerified)
+    content = (
+      <Row align="middle" justify="center" className={classes.RowVH}>
+        <Form className={classes.FormSize} onFinish={submitFormData}>
+          <Title level={3}>Reset Password</Title>
+
+          {/* For password */}
+          <InputPassword value={values.password} handleChange={handleChange} />
+
+          {/* For Password Confirm */}
+          <InputPasswordConfirm
+            value={values.passwordConfirm}
+            handleChange={handleChange}
+          />
+
+          {/* For Alert */}
+          <div id="alert" className={classes.AlertMargin}></div>
+
+          {/* Sign up button */}
+          <Button type="primary" htmlType="submit" block loading={loading}>
+            Update Password
+          </Button>
+        </Form>
+      </Row>
+    );
+
+  return <>{content}</>;
+};
+
+export default withRouter(ResetPassword);
---
+++ client/src/Containers/Authentication/VerifyAccount/VerifyAccount.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import { Alert, Row } from "antd";
+
+import classes from "./VerifyAccount.module.css";
+
+const VerifyAccount = (props) => {
+  useEffect(() => {
+    try {
+      const verifyToken = async () => {
+        try {
+          const result = await axios.post(
+            `/api/v1/auth/verify/${props.match.params.token}`
+          );
+
+          if (result.data.status === "success") {
+            ReactDOM.render(
+              <Alert
+                message="Success"
+                description={result.data.message}
+                type="success"
+                showIcon
+              />,
+              document.getElementById("alert")
+            );
+
+            setTimeout(() => {
+              window.location.assign("/login");
+            }, 2000);
+          }
+        } catch (error) {
+          ReactDOM.render(
+            <Alert
+              message="Failed"
+              description="Account token is not valid!"
+              type="error"
+              showIcon
+            />,
+            document.getElementById("alert")
+          );
+
+          setTimeout(() => {
+            window.location.assign("/signup");
+          }, 2000);
+        }
+      };
+
+      verifyToken();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [props.match.params.token]);
+
+  return (
+    <Row justify="center" align="middle" className={classes.RowVH}>
+      <div id="alert"></div>
+    </Row>
+  );
+};
+
+export default withRouter(VerifyAccount);
