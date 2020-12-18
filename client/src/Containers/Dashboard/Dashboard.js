3411691c920b5390285652781a0c93acc95d62d0
Joshua Corpin <jkcorpin@gmail.com>
2020-12-18 15:54:01 +0800

    Cleanup

--- client/src/Containers/Dashboard/Dashboard.js
+++ client/src/Containers/Dashboard/Dashboard.js
@@ -71,7 +71,7 @@ const Dashboard = ({ content }) => {
           {/* Content */}
           <Content style={{ margin: "24px 16px 0" }}>{content}</Content>
           <Footer style={{ textAlign: "center" }}>
-            Corps Innovation © 2021
+            © CORPS. All Rights Reserved
           </Footer>
 
           <Layout style={{ padding: "0 24px 24px" }}></Layout>
