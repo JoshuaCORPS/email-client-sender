407e210966c0ff6542af91fbcc051e6482173e57
Joshua Corpin <jkcorpin@gmail.com>
2020-12-18 15:53:36 +0800

    Added input for contactnumber and address

--- client/src/Containers/Authentication/Signup/Signup.js
+++ client/src/Containers/Authentication/Signup/Signup.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import { Form, Button, Row, Typography, Alert } from "antd";
+import { Form, Button, Row, Typography, Alert, Col } from "antd";
 
 import { useForm } from "../../../hooks/useForm";
 import InputName from "../../../Components/Form/InputName/InputName";
 import InputEmail from "../../../Components/Form/InputEmail/InputEmail";
+import InputContactNumber from "../../../Components/Form/InputContactNumber/InputContactNumber";
 import InputPassword from "../../../Components/Form/InputPassword/InputPassword";
 import InputPasswordConfirm from "../../../Components/Form/InputPasswordConfirm/InputPasswordConfirm";
+import InputAddress from "../../../Components/Form/InputAddress/InputAddress";
 import classes from "./Signup.module.css";
 
 const { Title } = Typography;
@@ -16,8 +18,10 @@ const Signup = () => {
   const [values, handleChange] = useForm({
     name: "",
     email: "",
+    contactNumber: "",
     password: "",
     passwordConfirm: "",
+    address: "",
   });
   const [loading, setLoading] = useState(false);
 
@@ -64,13 +68,28 @@ const Signup = () => {
         {/* For Name */}
         <InputName value={values.name} handleChange={handleChange} />
 
-        {/* For Email */}
-        <InputEmail value={values.email} handleChange={handleChange} />
+        <Row gutter={16}>
+          <Col span={12}>
+            {/* For Email */}
+            <InputEmail value={values.email} handleChange={handleChange} />
+          </Col>
 
-        {/* For password */}
+          <Col span={12}>
+            {/* For Email */}
+            <InputContactNumber
+              value={values.contactNumber}
+              handleChange={handleChange}
+            />
+          </Col>
+        </Row>
+
+        {/* For Address */}
+        <InputAddress value={values.address} handleChange={handleChange} />
+
+        {/* For Password */}
         <InputPassword value={values.password} handleChange={handleChange} />
 
-        {/* For Password Confirm */}
+        {/* For Confirm Password */}
         <InputPasswordConfirm
           value={values.passwordConfirm}
           handleChange={handleChange}
