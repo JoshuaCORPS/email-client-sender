5862d53892a3d8d03c30f90220e01850f7ee04c4
Joshua Corpin <jkcorpin@gmail.com>
2020-12-18 15:55:52 +0800

    Added container for updating info

--- client/src/Containers/UpdateInfo/UpdateInfo.js
+++ client/src/Containers/UpdateInfo/UpdateInfo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import { Form, Row, Button, Typography, Alert, Col } from "antd";
