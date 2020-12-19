ebad4b2c8fdbec37236f5ba9a7a5b530be4bd77f
Joshua Corpin <jkcorpin@gmail.com>
2020-12-19 20:00:03 +0800

    Implemented file upload for updating info

--- client/src/Containers/UpdateInfo/UpdateInfo.js
+++ client/src/Containers/UpdateInfo/UpdateInfo.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import { Form, Row, Button, Typography, Alert, Col } from "antd";
+import {
+  Form,
+  Row,
+  Button,
+  Typography,
+  Alert,
+  Col,
+  Upload,
+  Avatar,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 import { useForm } from "../../hooks/useForm";
 import InputName from "../../Components/Form/InputName/InputName";
@@ -20,19 +30,28 @@ const UpdateInfo = () => {
     address: "",
   });
   const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [client, setClient] = useState({});
   const [form] = Form.useForm();
 
   const submitFormData = async () => {
     try {
       setLoading(true);
 
+      const fd = new FormData();
+      fd.append("name", values.name);
+      fd.append("email", values.email);
+      fd.append("contactNumber", values.contactNumber);
+      fd.append("address", values.address);
+      fd.append("photo", selectedFile);
+
       const updateClientInfo = await axios.patch(
         "/api/v1/auth/update-info",
-        values,
+        fd,
         { withCredentials: true }
       );
 
-      if (updateClientInfo.data.status === "success")
+      if (updateClientInfo.data.status === "success") {
         ReactDOM.render(
           <Alert
             message="Success"
@@ -42,11 +61,13 @@ const UpdateInfo = () => {
           />,
           document.getElementById("alert")
         );
+      }
 
       setLoading(false);
 
       setTimeout(() => {
         ReactDOM.render("", document.getElementById("alert"));
+        window.location.assign("/");
       }, 3000);
     } catch (error) {
       ReactDOM.render(
@@ -60,6 +81,24 @@ const UpdateInfo = () => {
         ReactDOM.render("", document.getElementById("alert"));
       }, 5000);
     }
+  };
+
+  const fileChangedHandler = (e) => {
+    setSelectedFile(e.file.originFileObj);
+    if (e.file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const parent = document.getElementById("clientphoto");
+        parent.getElementsByTagName("img")[0].src = event.target.result;
+      };
+      reader.readAsDataURL(e.file.originFileObj);
+    }
+  };
+
+  const dummyRequest = ({ file, onSuccess }) => {
+    setTimeout(() => {
+      onSuccess("ok");
+    }, 0);
   };
 
   useEffect(() => {
@@ -82,6 +121,7 @@ const UpdateInfo = () => {
             values.email = result.data.data.client.email;
             values.contactNumber = result.data.data.client.contactNumber;
             values.address = result.data.data.client.address;
+            setClient(result.data.data.client);
           }
         } catch (error) {
           window.location.assign("/login");
@@ -94,6 +134,7 @@ const UpdateInfo = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <Row justify="center" align="middle" className={classes.RowVH}>
       <Form className={classes.FormSize} onFinish={submitFormData} form={form}>
@@ -120,12 +161,28 @@ const UpdateInfo = () => {
         {/* For Client Address */}
         <InputAddress value={values.address} handleChange={handleChange} />
 
+        <Avatar
+          id="clientphoto"
+          src={`https://corps-sender.herokuapp.com/img/users/${client.photo}`}
+          style={{ marginRight: "2rem" }}
+          size={{ xs: 60, sm: 60, md: 60, lg: 60, xl: 60, xxl: 60 }}
+        />
+
+        <Upload
+          name="photo"
+          accept="image/*"
+          onChange={fileChangedHandler}
+          customRequest={dummyRequest}
+        >
+          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+        </Upload>
+
         {/* For Alert */}
         <div id="alert" className={classes.AlertMargin}></div>
 
         {/* For Submit Button */}
         <Button type="primary" htmlType="submit" block loading={loading}>
-          Add User
+          Update Information
         </Button>
       </Form>
     </Row>
