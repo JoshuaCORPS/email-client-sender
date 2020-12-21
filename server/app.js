c64cf076168a02737b9babb682de943ccb1a48e7
Joshua Corpin <jkcorpin@gmail.com>
2020-12-21 18:59:34 +0800

    Added worker that checks user billdate

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const schedule = require('node-schedule');
 
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
@@ -72,6 +73,60 @@ app.get('/api/v1/status', statusController.status);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+const User = require('./models/userModel');
+const sendEmail = require('./util/email');
+
+schedule.scheduleJob('*/5 * * * *', async () => {
+  const users = await User.find();
+  const dateNow = new Date(Date.now());
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const subject = `Your Bill for the Month of ${
+    monthNames[new Date().getMonth()]
+  }`;
+
+  for (let user of users) {
+    const userBillDate = new Date(user.billDate);
+    const billSentDate = new Date(user.billSentDate);
+
+    if (
+      userBillDate.getDate() === dateNow.getDate() &&
+      billSentDate.toLocaleDateString() !== dateNow.toLocaleDateString()
+    ) {
+      user.billSentDate = new Date(Date.now()).toLocaleDateString();
+      user.balance = user.balance + user.monthlyBill;
+      await user.save();
+
+      const message = `Dear ${user.name},\n\n\nWe're pleased to send your e-Bill with the following details:\n\nAmount to pay: ${user.balance}`;
+
+      await sendEmail({
+        from: `Sender <no-reply@sender.com>`,
+        email: user.email,
+        subject,
+        message,
+      });
+      console.log(
+        `e-bill sent to ${user.email} at ${new Date(
+          Date.now()
+        ).toLocaleDateString()}`
+      );
+    }
+  }
 });
 
 app.use(errorController);
