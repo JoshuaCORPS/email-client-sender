01b3ba2adbbbad56b194e429d01844ed1d1cf7b2
Joshua Corpin <jkcorpin@gmail.com>
2020-12-22 16:51:03 +0800

    Components for date input

---
+++ client/src/Components/Form/InputDate/InputDate.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Form, DatePicker } from "antd";
+
+const InputDate = ({ value, handleChange }) => {
+  return (
+    <>
+      {/* For Numbers */}
+      <Form.Item
+        name="itemDate"
+        rules={[{ required: true, message: "Please input Billing Date!" }]}
+      >
+        <DatePicker
+          value={value}
+          placeholder="Select Billing Date"
+          style={{ width: "100%" }}
+          onChange={handleChange}
+          format="MM/DD/YYYY"
+        />
+      </Form.Item>
+    </>
+  );
+};
+
+export default InputDate;
