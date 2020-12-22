d998cecc7a34abde8acc252b9572c9932037090b
Joshua Corpin <jkcorpin@gmail.com>
2020-12-22 13:57:15 +0800

    Refactor worker function

--- server/app.js
+++ server/app.js
@@ -16,11 +16,12 @@ const viewRouter = require('./routes/viewRoutes');
 const authRouter = require('./routes/authRoutes');
 const clientRouter = require('./routes/clientRoutes');
 const adminRouter = require('./routes/adminRoutes');
+const statusRouter = require('./routes/statusRoutes');
 
 const errorController = require('./controllers/errorController');
-const statusController = require('./controllers/statusController');
 
 const AppError = require('./util/appError');
+const checkBillingDate = require('./util/checkBillDate');
 
 const app = express();
 
@@ -69,64 +70,14 @@ app.use('/api/v1/view', viewRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/clients', clientRouter);
 app.use('/api/v1/admin', adminRouter);
-app.get('/api/v1/status', statusController.status);
+app.use('/api/v1/status', statusRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-const User = require('./models/userModel');
-const sendEmail = require('./util/email');
-
 schedule.scheduleJob('*/5 * * * *', async () => {
-  const users = await User.find();
-  const dateNow = new Date(Date.now());
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const subject = `Your Bill for the Month of ${
-    monthNames[new Date().getMonth()]
-  }`;
-
-  for (let user of users) {
-    const userBillDate = new Date(user.billDate);
-    const billSentDate = new Date(user.billSentDate);
-
-    if (
-      userBillDate.getDate() === dateNow.getDate() &&
-      billSentDate.toLocaleDateString() !== dateNow.toLocaleDateString()
-    ) {
-      user.billSentDate = new Date(Date.now()).toLocaleDateString();
-      user.balance = user.balance + user.monthlyBill;
-      await user.save();
-
-      const message = `Dear ${user.name},\n\n\nWe're pleased to send your e-Bill with the following details:\n\nAmount to pay: ${user.balance}`;
-
-      await sendEmail({
-        from: `Sender <no-reply@sender.com>`,
-        email: user.email,
-        subject,
-        message,
-      });
-      console.log(
-        `e-bill sent to ${user.email} at ${new Date(
-          Date.now()
-        ).toLocaleDateString()}`
-      );
-    }
-  }
+  await checkBillingDate();
 });
 
 app.use(errorController);
