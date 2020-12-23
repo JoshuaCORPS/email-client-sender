d84cf6f7da8c977c9144bf8eeed79da8de5e83b8
Joshua Corpin <jkcorpin@gmail.com>
2020-12-23 20:09:51 +0800

    Refactored submitformdata function

--- client/src/Containers/Authentication/ResetPassword/ResetPassword.js
+++ client/src/Containers/Authentication/ResetPassword/ResetPassword.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
-import { Form, Button, Row, Typography, Alert } from "antd";
+import { Form, Button, Row, Typography } from "antd";
 
 import { useForm } from "../../../hooks/useForm";
+import { submitData } from "../../../util/submit-data";
 import InputPassword from "../../../Components/Form/InputPassword/InputPassword";
 import InputPasswordConfirm from "../../../Components/Form/InputPasswordConfirm/InputPasswordConfirm";
 import Spinner from "../../../Components/Spinner/Spinner";
@@ -21,54 +21,41 @@ const ResetPassword = (props) => {
     try {
       setLoading(true);
 
-      const searchClient = await axios.post(
-        `/api/v1/auth/reset-password/${props.match.params.token}`,
-        values
+      const endpoint = `/api/v1/auth/reset-password/${props.match.params.token}`;
+      const alertDesc =
+        "Your password has been reset! Please log in to continue.";
+      const setTimeoutFN = () => window.location.assign("/login");
+
+      await submitData(
+        "POST",
+        endpoint,
+        values,
+        {},
+        alertDesc,
+        setTimeoutFN,
+        3000
       );
 
-      if (searchClient.data.status === "success")
-        ReactDOM.render(
-          <Alert message="Success" type="success" showIcon />,
-          document.getElementById("alert")
-        );
-
       setLoading(false);
-
-      window.setTimeout(() => {
-        window.location.assign("/login");
-      }, 2000);
     } catch (error) {
-      ReactDOM.render(
-        <Alert message={error.response.data.message} type="error" showIcon />,
-        document.getElementById("alert")
-      );
-
-      setLoading(false);
-
-      setTimeout(() => {
-        ReactDOM.render("", document.getElementById("alert"));
-      }, 3000);
+      console.error(error.message);
     }
   };
 
   useEffect(() => {
-    try {
-      const verifyResetToken = async () => {
-        try {
-          const result = await axios.get(
-            `/api/v1/auth/reset-password/${props.match.params.token}`
-          );
+    const verifyResetToken = async () => {
+      try {
+        const result = await axios.get(
+          `/api/v1/auth/reset-password/${props.match.params.token}`
+        );
 
-          if (result.data.status === "success") setIsTokenVerified(true);
-        } catch (error) {
-          window.location.assign("/");
-        }
-      };
+        if (result.data.status === "success") setIsTokenVerified(true);
+      } catch (error) {
+        window.location.assign("/");
+      }
+    };
 
-      verifyResetToken();
-    } catch (error) {
-      console.log(error);
-    }
+    verifyResetToken();
   }, [props.match.params.token]);
 
   let content = <Spinner />;
