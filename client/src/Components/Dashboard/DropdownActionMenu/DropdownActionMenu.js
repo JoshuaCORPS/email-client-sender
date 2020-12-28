b805150c4fc9f3d537584ea67f869cb071cd9bf3
Joshua Corpin <jkcorpin@gmail.com>
2020-12-28 22:32:22 +0800

    Added delete handler

--- client/src/Components/Dashboard/DropdownActionMenu/DropdownActionMenu.js
+++ client/src/Components/Dashboard/DropdownActionMenu/DropdownActionMenu.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import { Menu } from "antd";
+import { Menu, message } from "antd";
+import { UserContext } from "../../../hooks/useCreateContext";
 
 const DropdownActionMenu = ({ record }) => {
+  const { client, setClient } = useContext(UserContext);
+
+  const handleDelete = async (e) => {
+    try {
+      await axios.delete(`/api/v1/clients/users/${record.id}`, {
+        withCredentials: true,
+      });
+
+      const filteredUsers = client.users.filter(
+        (user) => user._id !== record.id
+      );
+
+      const clientCopy = { ...client };
+      clientCopy.users = filteredUsers;
+
+      setClient(clientCopy);
+
+      message.success(`User ${record.name} deleted.`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Menu>
       <Menu.Item key="1">
         <Link to={`users/${record.id}/edit`}>Edit</Link>
       </Menu.Item>
-      <Menu.Item key="2">Delete</Menu.Item>
+      <Menu.Item key="2" onClick={handleDelete}>
+        Delete
+      </Menu.Item>
     </Menu>
   );
 };
