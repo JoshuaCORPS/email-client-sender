07b05961ab24d287064ad37ffe72dc19452a8eb2
Joshua Corpin <jkcorpin@gmail.com>
2021-01-04 15:08:57 +0800

    Added route for categerories requests

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const authController = require('../controllers/authController');
 const clientController = require('../controllers/clientController');
+const categoryController = require('../controllers/categoryController');
 
 const Router = express.Router();
 
@@ -13,9 +14,17 @@ Router.route('/users')
   .get(clientController.getAllUsers)
   .post(clientController.addUser);
 
+Router.route('/categories')
+  .get(categoryController.getAllCategories)
+  .patch(categoryController.addCategory);
+
 Router.route('/users/:userid')
   .get(clientController.getUser)
   .patch(clientController.updateUser)
   .delete(clientController.deleteUser);
+
+Router.route('/categories/:category')
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
 
 module.exports = Router;
