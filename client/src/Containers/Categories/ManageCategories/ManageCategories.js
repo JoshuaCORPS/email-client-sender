ee827a32033fb1a3719b4b78782d26dc7932295f
Joshua Corpin <jkcorpin@gmail.com>
2021-01-06 17:30:25 +0800

    Refactored container

--- client/src/Containers/Categories/ManageCategories/ManageCategories.js
+++ client/src/Containers/Categories/ManageCategories/ManageCategories.js
@@ -1,23 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import {
-  Table,
-  Space,
-  Button,
-  Divider,
-  Input,
-  Popconfirm,
-  message,
-} from "antd";
-import {
-  TableOutlined,
-  CheckCircleTwoTone,
-  CloseCircleTwoTone,
-} from "@ant-design/icons";
+import { Table, Input, message } from "antd";
+import { TableOutlined } from "@ant-design/icons";
 
 import { UserContext } from "../../../hooks/useCreateContext";
-import numberFormatter from "../../../util/numberFormatter";
+import { categoriesColumns, categoriesSubColumns } from "../../../util/columns";
 import classes from "./ManageCategories.module.css";
 
 const ManageCategories = () => {
@@ -75,82 +62,11 @@ const ManageCategories = () => {
     }
   };
 
-  const columns = [
-    {
-      title: "Category",
-      dataIndex: "category",
-      sorter: (a, b) => a.category.localeCompare(b.category),
-    },
-    {
-      title: "Actions",
-      dataIndex: "actions",
-      render: (_, record, _2) => (
-        <Space size="small">
-          <Button type="link">
-            <Link to={`categories/${record.key}/edit`}>Edit</Link>
-          </Button>
-          <Divider type="vertical" />
-          <Popconfirm
-            title="Are you sure you want to delete this category?"
-            okText="Yes"
-            cancelText="No"
-            onConfirm={(e) => handleDelete(record)}
-          >
-            <Button type="link">Delete</Button>
-          </Popconfirm>
-        </Space>
-      ),
-    },
-  ];
+  const cColumns = categoriesColumns(handleDelete);
+  const cSubColumns = categoriesSubColumns(classes);
 
-  const subColumns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      sorter: (a, b) => a.name.localeCompare(b.name),
-    },
-    {
-      title: "Monthly Bill",
-      dataIndex: "monthlyBill",
-      sorter: (a, b) => a.monthlyBill - b.monthlyBill,
-      render: (monthlyBill) => <>{`₱${numberFormatter(monthlyBill)}`}</>,
-    },
-    {
-      title: "Billing Date",
-      dataIndex: "billDate",
-      sorter: (a, b) => new Date(a.billDate) - new Date(b.billDate),
-      render: (billDate) => <>{new Date(`${billDate}`).toLocaleDateString()}</>,
-    },
-    {
-      title: "Paid",
-      dataIndex: "paid",
-      sorter: (a, b) => a.paid - b.paid,
-      render: (paid) => (
-        <>
-          {paid === true ? (
-            <CheckCircleTwoTone
-              className={classes.PaidIconSize}
-              twoToneColor="#52c41a"
-            />
-          ) : (
-            <CloseCircleTwoTone
-              className={classes.PaidIconSize}
-              twoToneColor="#D42322"
-            />
-          )}
-        </>
-      ),
-    },
-    {
-      title: "Balance",
-      dataIndex: "balance",
-      sorter: (a, b) => a.balance - b.balance,
-      render: (balance) => <>{`₱${numberFormatter(balance)}`}</>,
-    },
-  ];
-
-  const tableColumns = columns.map((item) => ({ ...item }));
-  const tableSubColumns = subColumns.map((item) => ({ ...item }));
+  const tableColumns = cColumns.map((item) => ({ ...item }));
+  const tableSubColumns = cSubColumns.map((item) => ({ ...item }));
   const expandable = {
     expandedRowRender: (record) => {
       const data = record.users.map((user) => ({
@@ -178,6 +94,7 @@ const ManageCategories = () => {
         expandable={expandable}
         dataSource={tableData}
         columns={tableColumns}
+        scroll={{ x: "100vw" }}
         bordered
       />
     </>
