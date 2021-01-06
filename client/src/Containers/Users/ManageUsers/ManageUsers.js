8ad4aba72319a2bcde1332e885c7966b760d43d9
Joshua Corpin <jkcorpin@gmail.com>
2021-01-06 17:30:42 +0800

    Refactored container

--- client/src/Containers/Users/ManageUsers/ManageUsers.js
+++ client/src/Containers/Users/ManageUsers/ManageUsers.js
@@ -1,16 +1,10 @@
 import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
-import { Table, Space, Dropdown, Button, message, Divider, Input } from "antd";
-import {
-  CheckCircleTwoTone,
-  CloseCircleTwoTone,
-  DownOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { Table, message, Input } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 import { UserContext } from "../../../hooks/useCreateContext";
-import numberFormatter from "../../../util/numberFormatter";
-import DropdownActionMenu from "../../../Components/Dashboard/DropdownActionMenu/DropdownActionMenu";
+import { usersColumns } from "../../../util/columns";
 import classes from "./ManageUsers.module.css";
 
 const ManageUser = () => {
@@ -73,96 +67,8 @@ const ManageUser = () => {
     }
   };
 
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      sorter: (a, b) => a.name.localeCompare(b.name),
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-    },
-    {
-      title: "Contact Number",
-      dataIndex: "contactNumber",
-    },
-    {
-      title: "Monthly Bill",
-      dataIndex: "monthlyBill",
-      sorter: (a, b) => a.monthlyBill - b.monthlyBill,
-      render: (monthlyBill) => <>{`₱${numberFormatter(monthlyBill)}`}</>,
-    },
-    {
-      title: "Billing Date",
-      dataIndex: "billDate",
-      sorter: (a, b) => new Date(a.billDate) - new Date(b.billDate),
-      render: (billDate) => <>{new Date(`${billDate}`).toLocaleDateString()}</>,
-    },
-    {
-      title: "Paid",
-      dataIndex: "paid",
-      sorter: (a, b) => a.paid - b.paid,
-      render: (paid) => (
-        <>
-          {paid === true ? (
-            <CheckCircleTwoTone
-              className={classes.PaidIconSize}
-              twoToneColor="#52c41a"
-            />
-          ) : (
-            <CloseCircleTwoTone
-              className={classes.PaidIconSize}
-              twoToneColor="#D42322"
-            />
-          )}
-        </>
-      ),
-    },
-    {
-      title: "Balance",
-      dataIndex: "balance",
-      sorter: (a, b) => a.balance - b.balance,
-      render: (balance) => <>{`₱${numberFormatter(balance)}`}</>,
-    },
-    {
-      title: "Category",
-      dataIndex: "billCategory",
-      filters:
-        client.billCategories &&
-        client.billCategories.map((category) => ({
-          text: category.value,
-          value: category.value,
-        })),
-      onFilter: (value, record) => record.billCategory.indexOf(value) === 0,
-    },
-    {
-      title: "Actions",
-      dataIndex: "actions",
-      render: (_, record, _2) => (
-        <Space size="small">
-          <Button
-            className={classes.ActionCol}
-            type="link"
-            onClick={(e) => handlePaid(record)}
-            disabled={record.paid}
-          >
-            Mark as Paid
-          </Button>
-
-          <Divider type="vertical" />
-
-          <Dropdown overlay={() => <DropdownActionMenu record={record} />}>
-            <Button className={classes.ActionMoreBtn} type="link">
-              More <DownOutlined />
-            </Button>
-          </Dropdown>
-        </Space>
-      ),
-    },
-  ];
-
-  const tableColumns = columns.map((item) => ({ ...item }));
+  const uColumns = usersColumns(classes, client, handlePaid);
+  const tableColumns = uColumns.map((item) => ({ ...item }));
 
   useEffect(() => {
     setClientDisplay(client);
@@ -181,8 +87,8 @@ const ManageUser = () => {
         className={classes.TableSize}
         columns={tableColumns}
         dataSource={tableDataFromClientState}
-        bordered
         scroll={{ x: "100vw" }}
+        bordered
       />
     </>
   );
