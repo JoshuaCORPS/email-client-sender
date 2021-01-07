bb485c9a8a3d1947746f6856c864fc7ea62d2344
Joshua Corpin <jkcorpin@gmail.com>
2021-01-07 16:22:46 +0800

    Added container for home view (charts)

---
+++ client/src/Containers/Dashboard/DashboardOverview/CardOverview/CardOverview.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, Typography } from "antd";
+import classes from "./CardOverview.module.css";
+
+const { Meta } = Card;
+const { Text } = Typography;
+
+const CardOverview = ({ title, description, Icon = "" }) => {
+  return (
+    <Card title={<Text type="secondary">{title}</Text>}>
+      <Meta
+        className={classes.CardTextSize}
+        avatar={Icon && <Icon />}
+        title={<span className={classes.CardTextSize}>{description}</span>}
+      />
+    </Card>
+  );
+};
+
+export default CardOverview;
---
+++ client/src/Containers/Dashboard/DashboardOverview/DashboardOvervew.js
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import { Row, Col } from "antd";
+import { UserOutlined, UnorderedListOutlined } from "@ant-design/icons";
+
+import { UserContext } from "../../../hooks/useCreateContext";
+import CanvasJSReact from "../../../util/assets/canvasjs.react";
+import numberFormatter from "../../../util/numberFormatter";
+import {
+  userCategorieOptions,
+  paidUsersOptions,
+} from "../../../util/charts-data";
+import CardOverview from "./CardOverview/CardOverview";
+import classes from "./DashboardOverview.module.css";
+
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const DashboardOvervew = () => {
+  const { client } = useContext(UserContext);
+
+  let expectedIncome = 0;
+
+  client.users &&
+    client.users.forEach(
+      (user) => (expectedIncome += user.monthlyBill + user.balance)
+    );
+
+  return (
+    <>
+      <Row gutter={16}>
+        <Col className={classes.CardMargin} xs={24} md={24} lg={8}>
+          <CardOverview
+            title="No. of Users"
+            description={client.users && client.users.length}
+            Icon={UserOutlined}
+          />
+        </Col>
+
+        <Col className={classes.CardMargin} xs={24} md={24} lg={8}>
+          <CardOverview
+            title="No. of Categories"
+            description={client.billCategories && client.billCategories.length}
+            Icon={UnorderedListOutlined}
+          />
+        </Col>
+
+        <Col className={classes.CardMargin} xs={24} md={24} lg={8}>
+          <CardOverview
+            title="Expected Income this Month"
+            description={`₱ ${numberFormatter(expectedIncome)}.00`}
+          />
+        </Col>
+      </Row>
+
+      <Row gutter={16}>
+        <Col className={classes.ChartSize} xs={24} md={12} lg={12}>
+          <CanvasJSChart options={userCategorieOptions(client)} />
+        </Col>
+
+        <Col className={classes.ChartSize} xs={24} md={12} lg={12}>
+          <CanvasJSChart options={paidUsersOptions(client)} />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default DashboardOvervew;
