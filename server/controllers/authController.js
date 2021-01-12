77a31c386da6f289ba92ed21d6210943775c334f
Joshua Corpin <jkcorpin@gmail.com>
2021-01-12 12:02:28 +0800

    Updated updateinfo handler to check contact number format

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -323,6 +323,9 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 });
 
 exports.updateInfo = catchAsync(async (req, res, next) => {
+  if (req.body.contactNumber && !req.body.contactNumber.startsWith('09'))
+    return next(new AppError("Contact number must start with '09...'", 400));
+
   const filteredBody = filterObj(
     req.body,
     'name',
