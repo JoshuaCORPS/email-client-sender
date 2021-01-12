14142c9fd9717a0fc60e49f19adb206e2304087f
Joshua Corpin <jkcorpin@gmail.com>
2021-01-12 14:02:12 +0800

    Cleanup

--- server/tests/api/auth/1.signup/signup.js
+++ server/tests/api/auth/1.signup/signup.js
@@ -1,287 +1,287 @@
-// const { expect } = require('chai');
-// const { MongoMemoryServer } = require('mongodb-memory-server');
-// const supertest = require('supertest');
-// const mongoose = require('mongoose');
+const { expect } = require('chai');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const supertest = require('supertest');
+const mongoose = require('mongoose');
 
-// const app = require('../../../../app');
-// require('dotenv').config();
+const app = require('../../../../app');
+require('dotenv').config();
 
-// const mongoServer = new MongoMemoryServer();
-// const request = supertest(app);
+const mongoServer = new MongoMemoryServer();
+const request = supertest(app);
 
-// describe('Auth Register API Endpoint', () => {
-//   const exec = (data) => {
-//     return request.post('/api/v1/auth/register').send(data);
-//   };
+describe('Auth Register API Endpoint', () => {
+  const exec = (data) => {
+    return request.post('/api/v1/auth/register').send(data);
+  };
 
-//   before(async () => {
-//     const mongoURI = await mongoServer.getUri();
-//     await mongoose.connect(mongoURI, {
-//       useCreateIndex: true,
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     });
-//   });
+  before(async () => {
+    const mongoURI = await mongoServer.getUri();
+    await mongoose.connect(mongoURI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  });
 
-//   after(async () => {
-//     await mongoose.disconnect();
-//   });
+  after(async () => {
+    await mongoose.disconnect();
+  });
 
-//   it('Ok, it should create a new client', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Ok, it should create a new client', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(201);
-//     expect(response.body.status).to.equal('success');
-//     expect(response.body).to.have.property('message');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(201);
+    expect(response.body.status).to.equal('success');
+    expect(response.body).to.have.property('message');
+  });
 
-//   it('Fail, it should NOT create a new client (duplicate)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (duplicate)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Email already exist.');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Email already exist.');
+  });
 
-//   it('Fail, It should NOT create a new client (no name)', async () => {
-//     const newClient = {
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, It should NOT create a new client (no name)', async () => {
+    const newClient = {
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Please provide your name');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Please provide your name');
+  });
 
-//   it('Fail, it should NOT create a new client (no email)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (no email)', async () => {
+    const newClient = {
+      name: 'test sender',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Please provide your email');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Please provide your email');
+  });
 
-//   it('Fail, it should NOT create a new client (invalid email format)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'invalid@email',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (invalid email format)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'invalid@email',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Please provide a valid email.');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Please provide a valid email.');
+  });
 
-//   it('Fail, it should NOT create a new client (no contactNumber)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (no contactNumber)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Please provide your contact number'
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Please provide your contact number'
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (contactNumber must start with 09...)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '12345678901',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (contactNumber must start with 09...)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '12345678901',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       "Contact number must start with '09...'"
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      "Contact number must start with '09...'"
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (contactNumber must only have 11 digits)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '0932165498',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (contactNumber must only have 11 digits)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '0932165498',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Contact number must only have 11 digits'
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Contact number must only have 11 digits'
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (contactNumber must only have 11 digits)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '093216549843',
-//       address: 'Marilao Bulacan',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (contactNumber must only have 11 digits)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '093216549843',
+      address: 'Marilao Bulacan',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Contact number must only have 11 digits'
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Contact number must only have 11 digits'
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (no address)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       password: 'testpassword',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (no address)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      password: 'testpassword',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.equal(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Please provide your address');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.equal(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Please provide your address');
+  });
 
-//   it('Fail, it should NOT create a new client (no password)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       passwordConfirm: 'testpassword',
-//     };
+  it('Fail, it should NOT create a new client (no password)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      passwordConfirm: 'testpassword',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       "Please provide your password.\nPassword don't match"
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      "Please provide your password.\nPassword don't match"
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (password must have atleast 8 char)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: '1234567',
-//       passwordConfirm: '1234567',
-//     };
+  it('Fail, it should NOT create a new client (password must have atleast 8 char)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: '1234567',
+      passwordConfirm: '1234567',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Password must have atleast 8 characters'
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Password must have atleast 8 characters'
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (must confirm password)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: '12345678',
-//     };
+  it('Fail, it should NOT create a new client (must confirm password)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: '12345678',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal('Please confirm your password');
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal('Please confirm your password');
+  });
 
-//   it("Fail, it should NOT create a new client (password don't match)", async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//       password: '12345678',
-//       passwordConfirm: '123456789',
-//     };
+  it("Fail, it should NOT create a new client (password don't match)", async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+      password: '12345678',
+      passwordConfirm: '123456789',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal("Password don't match");
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal("Password don't match");
+  });
 
-//   it('Fail, it should NOT create a new client (no password and passwordConfirm)', async () => {
-//     const newClient = {
-//       name: 'test sender',
-//       email: 'clientsender@mailsac.com',
-//       contactNumber: '09123465789',
-//       address: 'Marilao Bulacan',
-//     };
+  it('Fail, it should NOT create a new client (no password and passwordConfirm)', async () => {
+    const newClient = {
+      name: 'test sender',
+      email: 'clientsender@mailsac.com',
+      contactNumber: '09123465789',
+      address: 'Marilao Bulacan',
+    };
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Please confirm your password.\nPlease provide your password'
-//     );
-//   });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Please confirm your password.\nPlease provide your password'
+    );
+  });
 
-//   it('Fail, it should NOT create a new client (no data)', async () => {
-//     const newClient = {};
+  it('Fail, it should NOT create a new client (no data)', async () => {
+    const newClient = {};
 
-//     const response = await exec(newClient);
-//     expect(response.status).to.eq(400);
-//     expect(response.body.status).to.equal('fail');
-//     expect(response.body.message).to.equal(
-//       'Please confirm your password.\nPlease provide your password.\nPlease provide your address.\nPlease provide your contact number.\nPlease provide your email.\nPlease provide your name'
-//     );
-//   });
-// });
+    const response = await exec(newClient);
+    expect(response.status).to.eq(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.message).to.equal(
+      'Please confirm your password.\nPlease provide your password.\nPlease provide your address.\nPlease provide your contact number.\nPlease provide your email.\nPlease provide your name'
+    );
+  });
+});
