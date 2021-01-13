57bf73f98b95d478bace944f28ad7ab47fa2f03a
Joshua Corpin <jkcorpin@gmail.com>
2021-01-13 20:48:34 +0800

    Refactored test for login

--- server/tests/api/auth/2.login/login.js
+++ server/tests/api/auth/2.login/login.js
@@ -1,74 +1,55 @@
 const { expect } = require('chai');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const supertest = require('supertest');
-const mongoose = require('mongoose');
-
-const Client = require('../../../../models/clientModel');
-const app = require('../../../../app');
 require('dotenv').config();
 
-const mongoServer = new MongoMemoryServer();
-const request = supertest(app);
+const { login } = require('../../../util/response');
+const {
+  connect,
+  disconnect,
+  createClient,
+  deleteClients,
+} = require('../../../util/db');
 
 describe('Auth Login API Endpoint', () => {
-  const exec = (data) => {
-    return request.post('/api/v1/auth/login').send(data);
-  };
-
   before(async () => {
-    const mongoURI = await mongoServer.getUri();
-    await mongoose.connect(mongoURI, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-
-    await Client.create({
-      name: 'dummy sender',
-      email: 'dummysender@mailsac.com',
-      contactNumber: '09123465789',
-      address: 'Marilao Bulacan',
-      password: 'testpassword',
-      passwordConfirm: 'testpassword',
-      active: true,
-    });
+    await connect();
+    await createClient();
   });
 
   after(async () => {
-    await mongoose.disconnect();
+    await deleteClients();
+    await disconnect();
   });
 
   it('Ok, it should login the client', async () => {
-    clientInfo = {
+    const clientInfo = {
       email: 'dummysender@mailsac.com',
       password: 'testpassword',
     };
 
-    const response = await exec(clientInfo);
+    const response = await login(clientInfo);
     expect(response.status).to.equal(200);
     expect(response.body.status).to.equal('success');
     expect(response.body.data).to.have.property('client');
   });
 
   it('Fail, it should NOT login (wrong password)', async () => {
-    clientInfo = {
+    const clientInfo = {
       email: 'dummysender@mailsac.com',
       password: 'wrongpassword',
     };
 
-    const response = await exec(clientInfo);
+    const response = await login(clientInfo);
     expect(response.status).to.equal(401);
     expect(response.body.status).to.equal('fail');
     expect(response.body.message).to.equal('Incorrect email or password');
   });
 
   it('Fail, it should NOT login (no email)', async () => {
-    clientInfo = {
+    const clientInfo = {
       password: 'wrongpassword',
     };
 
-    const response = await exec(clientInfo);
+    const response = await login(clientInfo);
     expect(response.status).to.equal(400);
     expect(response.body.status).to.equal('fail');
     expect(response.body.message).to.equal(
@@ -77,11 +58,11 @@ describe('Auth Login API Endpoint', () => {
   });
 
   it('Fail, it should NOT login (no password)', async () => {
-    clientInfo = {
+    const clientInfo = {
       email: 'dummysender@mailsac.com',
     };
 
-    const response = await exec(clientInfo);
+    const response = await login(clientInfo);
     expect(response.status).to.equal(400);
     expect(response.body.status).to.equal('fail');
     expect(response.body.message).to.equal(
@@ -92,7 +73,7 @@ describe('Auth Login API Endpoint', () => {
   it('Fail, it should NOT login (no data)', async () => {
     const clientInfo = {};
 
-    const response = await exec(clientInfo);
+    const response = await login(clientInfo);
     expect(response.status).to.equal(400);
     expect(response.body.status).to.equal('fail');
     expect(response.body.message).to.equal(
