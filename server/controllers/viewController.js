c7841a1f062e30bcf43fa654042dbe84b3e361ab
Joshua Corpin <jkcorpin@gmail.com>
2021-01-23 13:37:28 +0800

    Implement caching in view handler

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,6 +1,12 @@
 const catchAsync = require('../util/catchAsync');
+const Client = require('../models/clientModel');
 
 exports.checkTokenAndSearchClient = catchAsync(async (req, res, next) => {
+  // for caching
+  await Client.findById(req.client.id).cache({
+    key: req.client.id,
+  });
+
   res.status(200).json({
     status: 'success',
     data: {
